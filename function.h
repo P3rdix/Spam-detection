8efd4ea4e80f24339957dd6dc4566945cdbcbd90
Vedant Nair <vedantxiii@gmail.com>
2023-04-10 17:47:30 +0530

    First working model complete

--- function.h
+++ function.h
@@ -3,6 +3,7 @@
 #include "spoof_data.h"
 
 float risk(struct data*,struct data*,int,int,int);
+float unlikeliness(struct call*,struct call*,int);
 
 char** sort_calls(struct data* data,int size, int day, int hour, int time){
     char** list = (char**)malloc(sizeof(char*)*size);
@@ -11,10 +12,11 @@ char** sort_calls(struct data* data,int size, int day, int hour, int time){
         p[i] = -1;
         *(list + i)= NULL;
     }
-int start_time = day*24 + hou;
+int start_time = day*24 + hour;
 
     for(int i=0;i<size;i++){
         float r = risk(data,(data+i),size,start_time,time);
+        printf("%f  %s\n",r,(data+i)->number);
         int t=0;
         while(p[t]<r && p[t]!=-1){
             t++;
@@ -36,49 +38,45 @@ int start_time = day*24 + hou;
     return list;
 }
 
-float risk(struct data* a, struct data* b, int size, int base, int disp){
+float risk(struct data* pg, struct data* b, int size, int base, int disp){
     /* iterate through calls in time base to base + disp
     return the most suspicious call
     *** Maybe ask return the indices of the call?
     */
-   int r = 0;
-   float e = 1;
-
-   for(int i=base;i<base+disp;i++){
-        float a = unlikeliness((b->log),(b->log)+i,i);
-        if(a<e){
-            e = a;
-            r = i;
+    int r = 0;
+    int d = 0;
+    float e = 1;
+    struct call* t = NULL;
+    for(int i=base;i<base+disp;i++){
+        struct call* t = (b)->log+i;
+        int w = 0;
+        while(t!=NULL){
+            float a = unlikeliness((b->log),t,i);
+            if(a<e){
+                e = a;
+                r = i;
+                d = w;
+            }
+            t = t->next;
+            w++;
         }
+        
    }
-   return recommendation(call at e);
+   return e;
 }
 
-float unlikeliness(struct call calls_made,struct call start, int base, int start_length){
-    int p[start_length];
+float unlikeliness(struct call* call_log,struct call* c, int base){
+    int p=0;
     int max=0;
-    for(int i=0;i<start_length;i++){p[i]=0;}
     for(int i=0;i<base;i++){
-        struct call temp = calls_made+i;
+        struct call* temp = call_log+i;
         while(temp!=NULL){
-            int r = contains(temp->recipient,start);
-            if (r>0){
-                p[r]+=1;
+            if(temp->recipient == c->recipient){
+                p++;
             }
             max++;
             temp = temp->next;
         }
     }
-}
-
-int contains(struct data c, struct call p){
-    int i=0;
-    while(p!=NULL){
-        if (p->recipient ==c){
-            return i; 
-        }
-        p= p->next;
-        i++;
-    }
-    return -1;
+    return (float)p/max;
 }
